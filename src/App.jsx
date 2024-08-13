4bbf5d07c4e2f982a3dbea69ef1d76d9b236f19e
James W <jamesweber1994@gmail.com>
2024-08-13 23:04:21 +1000

    Added GA support

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,14 @@
 import WikiTyper from "./components/WikiTyper.jsx";
+import ReactGA from "react-ga4";
+import React, { useEffect } from "react";
+
+ReactGA.initialize("G-G920F54PPT");
 
 function App() {
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+  }, []);
+
   return (
     <>
       <WikiTyper />
